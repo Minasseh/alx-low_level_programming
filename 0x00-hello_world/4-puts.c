a36e6a6ece1465774433a666130d3be9ec46ff2d
Minasseh <mnsalaro@yahoo.com>
2021-08-26 21:24:02 -0700

    puts on

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - accepts string
+ *
+ * Return: returns zero value
+ */
+int main(void)
+{
+  char str [] ="\"Programming is like building a multilingual puzzle";
+  puts(str);
+  return (0);
+}

8204d46b00fceda2ce50af0a5771d9fa7a91a667
Minasseh <mnsalaro@yahoo.com>
2021-08-27 00:28:27 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - shows the input
+ *
+ * Return: returns one
+ */
+int main()
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1)
+}

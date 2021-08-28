07bd77cfa2fcf565b447b1419baf7188c2073ace
Minasseh <mnsalaro@yahoo.com>
2021-08-28 08:31:22 -0700

    positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,8 +10,12 @@
 int main(void)
 {
   int n;
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
   printf("%2d is positive\n",n>0);
   printf("%2d is zero\n",n==0);
-  printf("%2d is negative\n",n<0);
+  printf("%2d is negative\n",n<0);{
+    putchar('n');
+    }
   return (0);
 }

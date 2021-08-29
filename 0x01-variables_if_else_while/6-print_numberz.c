bd7c983de9e69beb7f489a46c66c20cd8edda97d
Minasseh <mnsalaro@yahoo.com>
2021-08-29 11:35:50 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints numbers from 0-9
+ *
+ * Return: returns zero value
+ */
+int main(void)
+{
+int nu;
+for (nu = '0'; nu <= '9'; nu++)
+putchar(nu);
+putchar('\n');
+return (0);
+}

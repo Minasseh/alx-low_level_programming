ddbe0a5a714c284470e0ead1fafa83fe4bdb33e8
Minasseh <mnsalaro@yahoo.com>
2021-08-29 08:26:04 -0700

    grep

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints alphabets without e&q
+ *
+ * Return: returns zero value
+ */
+int main(void)
+{
+char alph;
+for (alph = 'a'; alph <= 'z'; alph++)
+putchar(alph); | grep eq;
+putchar('\n');
+return (0);
+}

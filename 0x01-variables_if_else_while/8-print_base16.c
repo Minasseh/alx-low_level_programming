175e22dc56d1f825d56a4f31ca1ccde94962af8f
Minasseh <mnsalaro@yahoo.com>
2021-08-30 05:01:40 -0700

    base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,10 +9,10 @@ int main(void)
 {
 char hexa;
 
-for(hexa = '0'; hexa <= '9'; hexa++)
+for (hexa = '0'; hexa <= '9'; hexa++)
 putchar(hexa);
 
-for(hexa = 'a'; hexa <= 'f'; hexa++)
+for (hexa = 'a'; hexa <= 'f'; hexa++)
 putchar(hexa);
 
 putchar('\n');

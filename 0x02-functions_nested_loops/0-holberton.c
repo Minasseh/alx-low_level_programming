ab5f191417d161e0912cac659c361347f45c6c4d
Minasseh <mnsalaro@yahoo.com>
2021-08-31 05:02:53 -0700

    holbrton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - prints Holberto and new line
+ *
+ * Return: returns zero value
+ */
+
+int main(void)
+{
+  int ho = Holberton;
+  putchar(ho);
+  putchar('\n')
+  return (0);
+}

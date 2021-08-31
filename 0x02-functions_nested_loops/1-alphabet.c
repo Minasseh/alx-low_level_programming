008f1eef06cd0ad6217537714a17e31f6edf39c4
Minasseh <mnsalaro@yahoo.com>
2021-08-31 15:06:31 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+print_alphabet();
+ for (alph = 'a'; alph <= 'z'; alph++)
+   _putchar(alph);
+ _putchar('\n');
+
+
+ return (0);
+}

5f2335a3165f6f371ff8a3d95fcc38e472953398
Minasseh <mnsalaro@yahoo.com>
2021-09-02 13:17:27 -0700

    largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,16 +14,16 @@ int largest_number(int a, int b, int c)
 
   if (a > b && b > c)
     {
-      largest == a;
+      largest = a;
     }
   else if (b > a && a > c)
     {
-      largest == b;
+      largest = b;
     }
   else
     {
-      largest == c;
+      largest = c;
     }
-
-  return (largest);
+  printf("%d is largest\n", largest);
+  return (0);
 }

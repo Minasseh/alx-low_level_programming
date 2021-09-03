f12fbcc33e7db97dc8eff0ca43404609423955e4
Minasseh <mnsalaro@yahoo.com>
2021-09-03 13:22:32 -0700

    upper

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
@@ -12,5 +12,5 @@ int i;
 i = 98;
 positive_or_negative(i);
 
-return (i);
+return (0);
 }

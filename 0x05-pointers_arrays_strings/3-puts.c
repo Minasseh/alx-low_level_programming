bd547cb252504c20a8ff3003cc48c354ae344a37
Minasseh <mnsalaro@yahoo.com>
2021-09-07 12:38:02 -0700

    string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts - displays the string into stdout
+ * @str: the given string
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+  char *str;
+  str = " ";
+  return 0;
+}

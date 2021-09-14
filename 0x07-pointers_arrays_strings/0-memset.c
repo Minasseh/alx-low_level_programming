9e7af2b58159734a1364f7580d11db9c951970cc
Minasseh <mnsalaro@yahoo.com>
2021-09-14 05:31:43 -0700

    memory set

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,6 +11,6 @@ char *_memset(char *s, char b, unsigned int n)
   unsigned int i;
   unsigned char *memory = s value = b;
   for(i = 0; i < n; i++)
-  memory[i] = value;
-  return (memory);
+  memset[i] = value;
+  return (value);
 }

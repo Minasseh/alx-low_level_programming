ecaa35d2329e5b25c033f1329f8ac372be92423d
Minasseh <mnsalaro@yahoo.com>
2021-09-14 04:29:05 -0700

    memory set

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _memset - sets constant byte b in memory
+ * @memory - pointes s
+ *
+ * Return: returns the value
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+  unsigned int i = 0;
+  unsigned char *memory = s;
+  for(i < n, i++);
+  memory[i] = b;
+  return (memory);
+}

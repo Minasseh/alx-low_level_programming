6a3bf0ad8126a743dd23334dc06173100d9236ea
Minasseh <mnsalaro@yahoo.com>
2021-09-16 02:39:41 -0700

    factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,21 +2,21 @@
 
 /**
  * factorial - re factorial of a number
- * @
+ * @n: the number
  * Return: factorial of n
  */
 
 int factorial(int n)
-{
-  int fact;
-  fact = n * factorial(n - 1);
-  return (fact);
-  if (n == 0)
-    {
-      return (1);
-    }
-  if (n < 0)
-    {
+  {
+    int result = n;
+
+    if (n < 0)
       return (-1);
-    }
-}
+
+    else if (n >= 0 && n <= 1)
+      return (1);
+
+    result *= factorial(n - 1);
+
+    return (result);
+  }

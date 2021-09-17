07a9780c13e826a402a7661e12ad0e7ecd463e23
Minasseh <mnsalaro@yahoo.com>
2021-09-16 23:56:41 -0700

    everything

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib>
+#include <stdio.h>
+
+/**
+ * main - function that accepts arguments
+ * @argc: number of args
+ * @argv: array of strings
+ * Return: 0
+ */
+
+int main(int argc, char* argv[])
+{
+  int i;
+  for (i = 0; i < argc; i++)
+    {
+      printf("%s\n", argv[i]);
+    }
+}

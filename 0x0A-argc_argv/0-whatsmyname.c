3e0d1d7549a324e248818332e3cc4bd36e4216cb
Minasseh <mnsalaro@yahoo.com>
2021-09-22 01:45:48 -0700

    My name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints name of the program
+ * @argc: arguments
+ * @argv: array of pointers
+ * Return: zero
+ */
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+
+return (0);
+}

2a509f44778f00f1dad5e2f7883c27f60dc2f967
Minasseh <mnsalaro@yahoo.com>
2022-07-01 08:19:28 -0700

    Patience

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,20 +5,27 @@
  *
  * Return: returns zero value
  */
+
 int main(void)
 {
-int number;
+	int number;
 
-for (number = 0; number <= 9; number++)
-{
-putchar(number + '0');
-if (number < 9)
-{
-putchar(',');
-putchar(' ');
-}
-}
-putchar('\n');
+	for (number = 0; number <= 9; number++)
+	{
 
-return (0);
+		putchar(number + '0');
+
+		if (number < 9)
+		{
+
+			putchar(',');
+
+			putchar(' ');
+		}
+
+	}
+
+	putchar('\n');
+
+	return (0);
 }

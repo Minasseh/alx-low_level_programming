c80ccb947b7b113a93d261873ff37fa22150cb56
Minasseh <mnsalaro@yahoo.com>
2022-07-01 07:48:04 -0700

    alphaBET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,13 +4,20 @@
  *
  * Return: returns zero
  */
+
 int main(void)
 {
-char alph;
-for (alph = 'a'; alph <= 'z'; alph++)
-putchar(alph);
-for (alph = 'A'; alph <= 'Z'; alph++)
-putchar(alph);
-putchar('\n');
-return (0);
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+
+		putchar(alph);
+
+	for (alph = 'A'; alph <= 'Z'; alph++)
+
+		putchar(alph);
+
+	putchar('\n');
+
+	return (0);
 }

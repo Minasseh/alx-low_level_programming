820a60fbcc364f6a356e124dc5cf47d9677fe79e
Minasseh <mnsalaro@yahoo.com>
2022-07-04 22:40:07 -0700

    Correction

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,15 @@
  *
  * Return: Always 0.
  */
+
 void print_alphabet(void)
 {
-char alph;
+	char alph;
 
-for (alph = 'a'; alph <= 'z'; alph++)
-_putchar(alph);
-_putchar('\n');
+	for (alph = 'a'; alph <= 'z'; alph++)
+
+		_putchar(alph);
+
+	_putchar('\n');
 
 }

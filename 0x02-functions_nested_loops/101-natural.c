3d801446946007edf25fc00d76ee54810b5f44ad
Minasseh <mnsalaro@yahoo.com>
2022-07-05 12:04:39 -0700

    Nature made the natural numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,17 +7,21 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-int i, sum = 0;
 
-for (i = 0; i < 1024; i++)
-{
-if ((i % 3) == 0 || (i % 5) == 0)
-sum += i;
-}
+	int i, sum = 0;
 
-printf("%d\n", sum);
+	for (i = 0; i < 1024; i++)
+	{
 
-return (0);
+		if ((i % 3) == 0 || (i % 5) == 0)
+
+			sum += i;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
 }

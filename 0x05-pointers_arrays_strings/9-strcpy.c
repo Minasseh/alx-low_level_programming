daae11e6fca3dd54531f8e4c271ee617eef55676
Minasseh <mnsalaro@yahoo.com>
2022-07-11 13:02:29 -0700

    String copy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,15 +8,18 @@
  *
  * Return: A pointer to the destination string @dest.
  */
+
 char *_strcpy(char *dest, const char *src)
 {
-int index = 0;
 
-while (src[index])
-{
-dest[index] = src[index];
-index++;
-}
+	int index = 0;
 
-return (dest);
+	while (src[index])
+	{
+
+		dest[index] = src[index];
+		index++;
+	}
+
+	return (dest);
 }

cab59be8af3d55aef815311931448396a69f2b1a
Minasseh <mnsalaro@yahoo.com>
2022-07-14 12:14:55 -0700

    Reverse array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -5,14 +5,17 @@
  * @a: The array of integers to be reversed.
  * @n: The number of elements in the array.
  */
+
 void reverse_array(int *a, int n)
 {
-int tmp, index;
 
-for (index = n - 1; index >= n / 2; index--)
-{
-tmp = a[n - 1 - index];
-a[n - 1 - index] = a[index];
-a[index] = tmp;
-}
+	int tmp, index;
+
+	for (index = n - 1; index >= n / 2; index--)
+	{
+
+		tmp = a[n - 1 - index];
+		a[n - 1 - index] = a[index];
+		a[index] = tmp;
+	}
 }

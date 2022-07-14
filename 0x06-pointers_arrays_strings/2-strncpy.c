79d4188987841984db5b2c2cd3c6e8435a78fd8a
Minasseh <mnsalaro@yahoo.com>
2022-07-14 12:05:39 -0700

    Copy string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,18 +9,20 @@
  *
  * Return: A pointer to the resulting string dest.
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
-int index = 0, src_len = 0;
 
-while (src[index++])
-src_len++;
+	int index = 0, src_len = 0;
 
-for (index = 0; src[index] && index < n; index++)
-dest[index] = src[index];
+	while (src[index++])
+		src_len++;
 
-for (index = src_len; index < n; index++)
-dest[index] = '\0';
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
 
-return (dest);
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
+
+	return (dest);
 }

cb68ad925fd7b989eba6132b686a6d3f0b917bcb
Minasseh <mnsalaro@yahoo.com>
2022-07-14 12:50:21 -0700

    Compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,9 +5,7 @@
  * @s1: A pointer to the first string to be compared.
  * @s2: A pointer to the second string to be compared.
  *
- * Return: If str1 < str2, the negative difference of the first unmatched chars.
- *         If str1 == str2, 0.
- *         If str1 > str2, the positive difference of the first unmatched chars
+ * Return: The difference
  */
 
 int _strcmp(char *s1, char *s2)

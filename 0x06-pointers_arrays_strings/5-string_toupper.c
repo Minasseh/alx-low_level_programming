d0ff709110f286c695d379721c318093e4f9edd9
Minasseh <mnsalaro@yahoo.com>
2022-07-14 12:17:41 -0700

    String to upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,17 +7,20 @@
  *
  * Return: A pointer to the changed string.
  */
+
 char *string_toupper(char *str)
 {
-int index = 0;
 
-while (str[index])
-{
-if (str[index] >= 'a' && str[index] <= 'z')
-str[index] -= 32;
+	int index = 0;
 
-index++;
-}
+	while (str[index])
+	{
 
-return (str);
+		if (str[index] >= 'a' && str[index] <= 'z')
+			str[index] -= 32;
+
+		index++;
+	}
+
+	return (str);
 }

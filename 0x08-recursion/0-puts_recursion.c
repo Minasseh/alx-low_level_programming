510796aa411b123d6efeb10dd587b43c5f6c02ca
Minasseh <mnsalaro@yahoo.com>
2023-06-07 17:15:36 -0700

    changes

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,11 +6,14 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-		return;
-	_putchar(*s);
-	_puts_recursion(++s);
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(++s);
+	}
 	else
-		_putchar('\0');
+	{
+		_putchar('\n');
+	}
 
 }
